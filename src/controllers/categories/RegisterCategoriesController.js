231c7ae6b529d1d8a544593c998ac93b996a41b8
Stormlig <114263838+Stormlig@users.noreply.github.com>
2023-10-22 22:51:46 -0300

    feat: add registrar categoria

---
+++ src/controllers/categories/RegisterCategoriesController.js
@@ -0,0 +1,29 @@
+const handleError = require("../../errors/handlerError");
+
+const {
+  RegisterCategoriesService,
+} = require("../../services/categories/index");
+
+class RegisterCategoriesController {
+  constructor() {
+    this.registerCategoriesService = new RegisterCategoriesService();
+  }
+  async RegisterCategories(request, response) {
+    const { userId } = response.locals.usuario;
+    const { name, description } = request.body;
+
+    try {
+      const result = await this.registerCategoriesService.execute(
+        name,
+        description,
+        userId,
+      );
+
+      return response.status(200).json(result);
+    } catch (error) {
+      handleError(response, error, 400);
+    }
+  }
+}
+
+module.exports = { RegisterCategoriesController };
---
+++ src/services/categories/RegisterCategoriesService.js
@@ -0,0 +1,32 @@
+const {
+  CategoriesRepository,
+} = require("../../database/Repositories/CategoriesRepository/index");
+
+const {
+  UserRepository,
+} = require("../../database/Repositories/UserRepository/index");
+
+const { Categories } = require("../../entities/categories/index");
+
+class RegisterCategoriesService {
+  constructor() {
+    this.CategoriesRepository = new CategoriesRepository();
+    this.userRepository = new UserRepository();
+  }
+  async execute(userId, name, description) {
+    const existingUser = await this.userRepository.findById(userId);
+
+    if (!existingUser) {
+      throw new Error("Usuário não encontrado");
+    }
+
+    const newCategories = new Categories(null, name, description);
+
+    const createdCategory =
+      await this.CategoriesRepository.create(newCategories);
+
+    return createdCategory;
+  }
+}
+
+module.exports = { RegisterCategoriesService };
