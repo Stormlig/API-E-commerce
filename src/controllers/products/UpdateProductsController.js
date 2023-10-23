74f4b1be378e3683c80f5ad9ec73bd1daabcb390
Stormlig <114263838+Stormlig@users.noreply.github.com>
2023-10-23 16:20:11 -0300

    feat: add middleware ao endPoint jwt

--- src/controllers/products/UpdateProductsController.js
+++ src/controllers/products/UpdateProductsController.js
@@ -8,10 +8,13 @@ class UpdateProductsController {
     this.updateProductsService = new UpdateProductsService();
   }
   async Update(request, response) {
+    const { userId } = response.locals.usuario;
+
     const { id, name, description, categories_id, mark, price } = request.body;
     // adicionar uma verificação requisição  que valida se  usuario tem permissão para editar os produtos.
     try {
       const product = await this.updateProductsService.execute({
+        userId,
         id,
         name,
         description,
--- src/services/products/UpdateProductsService.js
+++ src/services/products/UpdateProductsService.js
@@ -2,11 +2,22 @@ const {
   ProductsRepository,
 } = require("../../database/Repositories/ProductsRepository/index");
 
+const {
+  UserRepository,
+} = require("../../database/Repositories/UserRepository/index");
+
 class UpdateProductsService {
   constructor() {
     this.productsRepository = new ProductsRepository();
+    this.userRepository = new UserRepository();
   }
-  async execute({ id, name, description, categories_id, mark, price }) {
+  async execute({ userId, id, name, description, categories_id, mark, price }) {
+    const existingUser = await this.userRepository.findById(userId);
+
+    if (!existingUser) {
+      throw new Error("Usuário não encontrado");
+    }
+
     const product = await this.productsRepository.update({
       id,
       name,
