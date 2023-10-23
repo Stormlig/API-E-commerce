bea4e59fe8475c2a6cf040fe64eb617247e0931f
Stormlig <114263838+Stormlig@users.noreply.github.com>
2023-10-22 22:23:01 -0300

    feat: adiciona novas rotas ao arquivo

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
-const { router } = require("./routes/routes");
+const { router } = require("./routes/userRoutes/routes");
 const { FieldsValidator } = require("./middlewares/index");
+const { routerProducts } = require("./routes/productsRoutes/routes");
+const { routeSales } = require("./routes/salesRoutes/routes");
+const { routeStocks } = require("./routes/stocksRoutes/routes");
+const { routeCategories } = require("./routes/categoriesRoutes/routes");
+const { routeCarts } = require("./routes/cartsRoutes/routes");
 
 const app = express();
 const fieldsValidator = new FieldsValidator();
@@ -8,5 +13,10 @@ const fieldsValidator = new FieldsValidator();
 app.use(express.json());
 app.use(fieldsValidator.validateFields);
 app.use(router);
+app.use(routerProducts);
+app.use(routeSales);
+app.use(routeStocks);
+app.use(routeCategories);
+app.use(routeCarts);
 
 module.exports = { app };
