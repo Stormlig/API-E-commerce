163ba15778b2c2d79aff4bc2388dfce1c2dd29f7
Stormlig <114263838+Stormlig@users.noreply.github.com>
2023-10-22 22:56:10 -0300

    feat: add index de import

---
+++ src/services/stocks/index.js
@@ -0,0 +1,9 @@
+const { ListAllStocksService } = require("./ListAllStocksService");
+const { UpdateStocksService } = require("./UpdateStocksService");
+const { DeleteStockService } = require("./DeleteStockService");
+
+module.exports = {
+  ListAllStocksService,
+  UpdateStocksService,
+  DeleteStockService,
+};
