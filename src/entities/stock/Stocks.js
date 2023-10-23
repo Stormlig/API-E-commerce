e8f10ef62499a9d1683a011d53b1e2e1b985a8a0
Stormlig <114263838+Stormlig@users.noreply.github.com>
2023-10-22 23:02:17 -0300

    feat: add  entidade estoque

---
+++ src/entities/stock/Stocks.js
@@ -0,0 +1,10 @@
+class Stocks {
+  constructor(categories_id, product_id, quantity, name_category) {
+    this.categories_id = categories_id;
+    this.product_id = product_id;
+    this.quantity = quantity;
+    this.name_category = name_category;
+  }
+}
+
+module.exports = { Stocks };
