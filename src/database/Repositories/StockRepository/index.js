da159cfd7185cf7f00097c457e348ead98ec2b6f
Stormlig <114263838+Stormlig@users.noreply.github.com>
2023-10-22 22:56:42 -0300

    feat: add repositorio de estoque

---
+++ src/database/Repositories/StockRepository/index.js
@@ -0,0 +1,41 @@
+const db = require("../../connection");
+
+class StockRepository {
+  async register(productBox) {
+    await db("stocks").insert({
+      categories_id: productBox.categories_id,
+      product_id: productBox.product_id,
+      quantity: productBox.quantity,
+      name_category: productBox.name_category,
+    });
+  }
+
+  async findAll() {
+    const result = await db("stocks").select("*");
+
+    return result;
+  }
+
+  async findById(productId) {
+    const result = await db("stocks")
+      .where({ product_id: productId })
+      .first()
+      .returning("*");
+
+    return result;
+  }
+
+  async update(productBox) {
+    const result = await db("stocks")
+      .where({ product_id: productBox.product_id })
+      .update({
+        quantity: productBox.quantity,
+        name_category: productBox.name_category,
+      })
+      .returning("*");
+
+    return result;
+  }
+}
+
+module.exports = { StockRepository };
