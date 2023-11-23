d00f0f8c1f26f39e879a76c79693c0ac65f8a151
Stormlig <114263838+Stormlig@users.noreply.github.com>
2023-11-23 12:32:49 -0300

    feat: add import multer

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,9 @@
 const { AuthJsonWebTokenMiddleware } = require("./AuthenticationjsonWebToken");
 const { FieldsValidator } = require("./VerifyFields");
+const multer = require("./multer");
 
 module.exports = {
   FieldsValidator,
   AuthJsonWebTokenMiddleware,
+  multer,
 };
