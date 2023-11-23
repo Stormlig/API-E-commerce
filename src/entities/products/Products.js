910d83d000b5c90897517e1be7a6ec924b9c4cc7
Stormlig <114263838+Stormlig@users.noreply.github.com>
2023-11-23 12:33:25 -0300

    refactor: add  image a entity

--- src/entities/products/Products.js
+++ src/entities/products/Products.js
@@ -1,13 +1,14 @@
 const { UUIDGenerator } = require("../../utils/GeneratorUuid");
 
 class Products {
-  constructor(id, name, description, category, mark, price) {
+  constructor(id, name, description, category, mark, price, image) {
     this.id = id || new UUIDGenerator().generateUUID();
     this.name = name;
     this.description = description;
     this.categories_id = category;
     this.mark = mark;
     this.price = price;
+    this.image = image || null;
   }
 }
 
